55b4e694d54f0cd812fa9e99c7e375a000c690e3
ukhryab <mihail.sibirev@mail.ru>
2018-04-10 15:50:19 +0300

    Add some checkboxes handling.
    Add some docstrings.

--- boxes.py
+++ boxes.py
@@ -7,11 +7,13 @@ from buttons import MenuButton, ScriptButton
 
 
 class MenuBtnGroup(urwid.WidgetWrap):
+    """Menu button group."""
     def __init__(self, button_group):
         super().__init__(urwid.GridFlow(button_group, 20, 1, 1, 'center'))
 
 
 class Box(urwid.WidgetWrap):
+    """Base box class."""
     # TODO: rework params
     def __init__(self, title, text, frame, contents=None):
         self.body = [urwid.Text(title), urwid.Divider()]
@@ -29,10 +31,24 @@ class Box(urwid.WidgetWrap):
             urwid.ListBox(urwid.SimpleFocusListWalker(self.body)))
 
     def open(self, button, frame):
+        """
+        Calls parent frame method of displaying the box.
+        :param button:
+        :param frame: parent frame
+        :return: parent frame method call
+        """
         return frame.open_box(self)
+
+    def clear_edited(self):
+        """
+        Clears all editable fields (turns them to default state).
+        This method should be overrided in child classes.
+        """
+        raise NotImplementedError
 
 
 class SubMenu(Box):
+    """Represents submenu box."""
     def __init__(self, title, text, frame,
                  top_level=False,
                  contents=None,
@@ -42,10 +58,15 @@ class SubMenu(Box):
         self.actions = []
         self.parameters = []
         self.count_click_select_all = 0
+        self.checkbox_group = checkbox_group
+        self.elements = contents[:]
 
-        if checkbox_group:
+        if self.checkbox_group:
+            for item in self.elements:
+                urwid.connect_signal(item, 'change', self.checkbox_changed)
+
             self.actions.append(
-                MenuButton('Select all...', self.select_all, contents)
+                MenuButton('Select all...', self.select_all, self.elements)
             )
 
         self.actions.append(MenuButton('Back', frame.back))
@@ -63,6 +84,7 @@ class SubMenu(Box):
         super().__init__(title, text, frame, body)
 
     def select_all(self, button, choices):
+        """ Select all checkboxes. """
         for i in choices:
             if self.count_click_select_all % 2 == 0:
                 i.set_state(True)
@@ -70,8 +92,24 @@ class SubMenu(Box):
                 i.set_state(False)
         self.count_click_select_all += 1
 
+    def checkbox_changed(self, button, data=None):
+        """ Checkbox change event. """
+        if button.state:
+            self.parameters.append(button.label)
+        else:
+            try:
+                self.parameters.remove(button.label)
+            except ValueError:
+                pass
+
+    def clear_edited(self):
+        """ Clear editable fields. """
+        if self.checkbox_group:
+            [i.set_state(False) for i in self.elements if i.state]
+
 
 class ActionBox(Box):
+    """ Represents single action box with no child boxes. """
     def __init__(self, title, text, frame, contents=None, script=''):
         self.actions = []
         self.parameters = []
@@ -90,3 +128,7 @@ class ActionBox(Box):
         body.append(MenuBtnGroup(self.actions))
 
         super().__init__(title, text, frame, body)
+
+    def clear_edited(self):
+        """Do nothing, as this box does not have editable elements yet."""
+        pass
